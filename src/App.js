831f790cd9d26f6cdc3a284bc6d0c3fda6b50cc6
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-01-30 23:22:05 -0800

    skillex

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Content from "./Content";
+import Faq from "./Faq";
+import Header from "./Header";
+import Hero from "./Hero";
+import Instructor from "./Instructor";
+import OnlineCourses from "./OnlineCourses";
+import OurTopCategories from "./OurTopCategories";
+import Student from "./Student";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="content">
+        <Student />
+        <div>
+          <Hero />
+        </div>
+        <div className="online">
+          <OnlineCourses />
+        </div>
+        <div className="topcategories">
+          <OurTopCategories />
+        </div>
+        <div className="content">
+          <Content />
+        </div>
+        <div className="Instructor">
+          <Instructor />
+        </div>
+        <div className="faq">
+          <Faq />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,35 @@
+
+import { motion, useScroll, } from "framer-motion"
+
+const title = "SKILLEX"
+
+function Component() {
+    const { scrollYProgress } = useScroll();
+  
+    return(
+        <motion.div style={{ scaleX: scrollYProgress }} />
+        
+    )
+}
+
+const Header = () => {
+    return (
+        
+        <div className="header">
+            
+            <div className="list">
+                <ul>
+                    <li> <span><h1>{ title }</h1></span></li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Courses</li>
+                    <li>Teacher</li>
+                    <li>Contact</li>
+                    <li><span><button>Sign Up</button></span></li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
