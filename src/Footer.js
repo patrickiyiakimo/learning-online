c92b62d637f81d7687e3f6b5d2aabddc32424540
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-02-03 23:36:41 -0800

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,32 @@
 const Footer = () => {
   return (
     <div className="footer">
-      {/* <h4>Get Started</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-        doloremque blanditiis vel ea, repellat quam.
-      </p>
-      <h4>Services</h4>
-      <p>Web Design</p>
-      <p>Web Development</p>
-      <p>Science</p>
-      <h4>Company</h4>
-      <p>Privacy Policy</p>
-      <p>Sitemap</p>
-      <p>Careers</p>
-      <h4>Follow Us</h4>
-      <p>Skillexlearning@gmail.com</p>
-      <p>+123456789</p> */}
+      <div className="lister">
+        <h4>Get Started</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint facilis
+          magnam praesentium eum laudantium tenetur est aut natus distinctio!
+          Blanditiis fugit vel culpa veniam porro, officia est velit pariatur.
+          Laudantium.
+        </p>
+        <div className="content">
+          <h4>Services</h4>
+          <p>Web Design</p>
+          <p>Web Development</p>
+          <p>Science</p>
+        </div>
+        <div className="content-one">
+          <h4>Company</h4>
+          <p>Privacy Policy</p>
+          <p>Sitemap</p>
+          <p>Career</p>
+        </div>
+        <div className="content-two">
+          <h4>Follow Us</h4>
+          <p>Skillexlearning@gmail.com</p>
+          <p>+123456789</p>
+        </div>
+      </div>
     </div>
   );
 };
